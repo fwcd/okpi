95aa2b06b51b8be89dacd5bcbf33b4cbdf5a7720
fwcd <fwcdmail@gmail.com>
2019-01-22 20:32:05 +0100

    Repointed 'sopare' submodule to the fork repository with Python 3 support and added a WIP main script

--- src/main.py
+++ src/main.py
@@ -1,28 +1,24 @@
-import subprocess
-import speech_recognition
-import os
+import lib.sopare.sopare
 
-MODEL_FILE = "local/models/output_graph.pbmm"
-ALPHABET_FILE = "local/models/alphabet.txt"
-AUDIO_FILE = "local/input.wav"
-LANGUAGE_MODEL_FILE = "local/models/lm.binary"
-TRIE_FILE = "local/models/trie"
-
-def to_abs_path(rel_path):
-    abs_path = os.path.join(os.getcwd(), rel_path)
-    if not os.path.isfile(abs_path):
-        raise ValueError(abs_path + " file not found")
-    return abs_path
+def create_config(cfg_ini, endless_loop, debug, plot, wave, outfile, infile, dict, error):
+    if (cfg_ini == None):
+        cfg = sopare.config.config()
+    else:
+        cfg = sopare.config.config(cfg_ini)
+    logger = sopare.log.log(debug, error, cfg)
+    cfg.addsection('cmdlopt')
+    cfg.setoption('cmdlopt', 'endless_loop', str(endless_loop))
+    cfg.setoption('cmdlopt', 'debug', str(debug))
+    cfg.setoption('cmdlopt', 'plot', str(plot))
+    cfg.setoption('cmdlopt', 'wave', str(wave))
+    cfg.setoption('cmdlopt', 'outfile', outfile)
+    cfg.setoption('cmdlopt', 'infile', infile)
+    cfg.setoption('cmdlopt', 'dict', dict)
+    cfg.addlogger(logger)
+    return cfg
 
 def main():
-    
-    print("Output:", speech_recognition.speech_to_text(
-        model_file=to_abs_path(MODEL_FILE),
-        alphabet_file=to_abs_path(ALPHABET_FILE),
-        audio_file=to_abs_path(AUDIO_FILE),
-        language_model=to_abs_path(LANGUAGE_MODEL_FILE),
-        trie=to_abs_path(TRIE_FILE)
-    ))
+    sopare.main()
 
 if __name__ == "__main__":
     main()
