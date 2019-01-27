a6fdade53171ce717f5db198a17098fde6884af8
fwcd <fwcdmail@gmail.com>
2019-01-27 17:41:59 +0100

    Added path verification

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -7,8 +7,12 @@ function main(): void {
 	
 	// Assuming that the PocketSphinx repository is a sibling directory of "OkPi"
 	const psRepo = path.resolve(__dirname, "..", "..", "..", "pocketsphinx");
-	const modelDir = path.resolve(psRepo, "model", "en-us");
 	
+	if (!fs.existsSync(psRepo)) {
+		throw new Error("Could not find 'pocketsphinx' directory in the superdirectory of 'OkPi'. Try cloning it from GitHub: https://github.com/cmusphinx/pocketsphinx");
+	}
+	
+	const modelDir = path.resolve(psRepo, "model", "en-us");
 	const config = new ps.Decoder.defaultConfig();
 	config.setString("-hmm", modelDir + "en-us");
 	config.setString("-dict", modelDir + "cmudict-en-us.dict");
