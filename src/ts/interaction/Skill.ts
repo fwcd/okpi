2eeb319d7e37d97a620f5691a0bbd02957183264
fwcd <fwcdmail@gmail.com>
2019-01-29 20:30:19 +0100

    Extended Skill interface to provide utterances

--- src/ts/interaction/Skill.ts
+++ src/ts/interaction/Skill.ts
@@ -7,4 +7,15 @@ import { OutputFacade } from "./OutputFacade";
  */
 export interface Skill {
 	invoke(intent: Intent, out: OutputFacade): void;
+	
+	/**
+	 * Provides a list of template utterances
+	 * to match with, for example:
+	 * 
+	 * `set a timer for {minutes} minutes`
+	 * 
+	 * Parameter placeholders are enclosed in
+	 * curly braces.
+	 */
+	getUtterances(): string[];
 }
