2b8b0fa0ce4f63ead8c77fe8a7b134931db0b05b
fwcd <fwcdmail@gmail.com>
2019-01-29 20:48:19 +0100

    Implemented skill registration in the assistant layer

--- src/ts/assistant/OkPiAssistant.ts
+++ src/ts/assistant/OkPiAssistant.ts
@@ -1,11 +1,14 @@
 import { SpeechAssistant } from "./SpeechAssistant";
 import { SpeechRecognitionEngine } from "../speech/SpeechRecognitionEngine";
+import { Skill } from "../interaction/Skill";
+import { UtteranceProcessor } from "../nlu/UtteranceProcessor";
 
 /**
  * The default speech assistant built for this project.
  */
 export class OkPiAssistant implements SpeechAssistant {
 	private engine: SpeechRecognitionEngine;
+	private uttProcessor = new UtteranceProcessor();
 	
 	public constructor(engine: SpeechRecognitionEngine) {
 		this.engine = engine;
@@ -13,5 +16,13 @@ export class OkPiAssistant implements SpeechAssistant {
 	
 	public launch(): void {
 		this.engine.start();
+	}
+	
+	public registerSkills(...skills: Skill[]): void {
+		skills.forEach(skill => this.uttProcessor.register(skill));
+	}
+	
+	public unregisterSkills(...skills: Skill[]): void {
+		skills.forEach(skill => this.uttProcessor.unregister(skill));
 	}
 }
--- src/ts/assistant/SpeechAssistant.ts
+++ src/ts/assistant/SpeechAssistant.ts
@@ -1,3 +1,9 @@
+import { Skill } from "../interaction/Skill";
+
 export interface SpeechAssistant {
 	launch(): void;
+	
+	registerSkills(...skills: Skill[]): void;
+	
+	unregisterSkills(...skills: Skill[]): void;
 }
