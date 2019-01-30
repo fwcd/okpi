0c8b3a894e92f486b57f66a831e3c93ab0a70196
fwcd <fwcdmail@gmail.com>
2019-01-30 18:50:46 +0100

    Added stub implementation of DeepSpeechEngine

---
+++ src/ts/speech/DeepSpeechEngine.ts
@@ -0,0 +1,24 @@
+import { SpeechRecognitionEngine } from "./SpeechRecognitionEngine";
+import { TextOutput } from "../output/text/TextOutput";
+
+export class DeepSpeechEngine implements SpeechRecognitionEngine {
+	public setUtteranceOutput(output: TextOutput): void {
+		// TODO
+	}
+	
+	public setKeyphrase(keyphrase: string): void {
+		// TODO
+	}
+	
+	public getKeyphrase(): string {
+		// TODO
+	}
+	
+	public start(): void {
+		// TODO
+	}
+	
+	public stop(): void {
+		// TODO
+	}
+}
